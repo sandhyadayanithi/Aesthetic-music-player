a19f9efcdf1abced65d6e2f4536ab4befbcf7e43
Sandhya <sandhyadayanithi@gmail.com>
2025-11-29 20:22:46 +0530

    Initial commit

---
+++ info.js
@@ -0,0 +1,32 @@
+export const playlist=[
+  {
+    songName: "Willow",
+    artistName: "Taylor Swift",
+    albumCover: "images/willow.jpg",
+    track: "songs/Taylor Swift-Willow.mp3"
+  },
+  {
+    songName: "Attention",
+    artistName: "Charlie Puth",
+    albumCover: "images/attention.jpg",
+    track: "songs/Attention - Charlie Puth.mp3"
+  },
+  {
+    songName: "Ghost",
+    artistName: "Justin Bieber",
+    albumCover: "images/ghost.jpg",
+    track: "songs/Justin Bieber  Ghost.mp3"
+  },
+  {
+    songName: "Ours",
+    artistName: "Taylor Swift",
+    albumCover: "images/ours.jpg",
+    track: "songs/Taylor Swift-Ours(Taylor's Version).mp3"
+  },
+  {
+    songName: "Supernatural",
+    artistName: "Ariana Grande",
+    albumCover: "images/supernatural.jpg",
+    track: "songs/Ariana Grande - supernatural (Lyrics) (mp3cut.net).mp3"
+  }
+];
---
+++ script.js
@@ -0,0 +1,65 @@
+import { playlist } from "./info.js";
+
+const albumCover=document.querySelector('.album-cover');
+const songname=document.querySelector('.song-name');
+const artistName=document.querySelector('.artist-name');
+const track=document.querySelector('.track');
+
+const prev=document.querySelector('.prev-song');
+const next=document.querySelector('.next-song');
+const playPause=document.querySelector('.play-pause');
+const icon=document.querySelector('.play-pause i');
+const progress=document.querySelector('.progress');
+
+let index=0;
+let toggle=0;
+
+prev.addEventListener('click',()=>{
+  index=(index-1 + playlist.length) % playlist.length;
+  track.src=playlist[index].track;
+  albumCover.src=playlist[index].albumCover;
+  songname.textContent=playlist[index].songName;
+  artistName.textContent=playlist[index].artistName;
+  icon.classList.remove('fa-pause');
+  icon.classList.add('fa-play');
+});
+
+next.addEventListener('click',()=>{
+  index=(index+1) % playlist.length;
+  track.src=playlist[index].track;
+  albumCover.src=playlist[index].albumCover;
+  songname.textContent=playlist[index].songName;
+  artistName.textContent=playlist[index].artistName;
+  icon.classList.remove('fa-pause');
+  icon.classList.add('fa-play');
+});
+
+playPause.addEventListener('click',()=>{
+  if(!toggle){
+    track.currentTime=progress.value;
+    track.play();
+    icon.classList.replace('fa-play','fa-pause');
+    toggle=1;
+  }
+  else{
+    track.pause();
+    icon.classList.replace('fa-pause','fa-play');
+    toggle=0;
+  }
+});
+
+progress.addEventListener('input', () => {
+  track.currentTime = progress.value; 
+});
+
+track.onloadedmetadata=()=>{
+  progress.max=track.duration;
+  progress.value=track.currentTime;
+};
+
+track.addEventListener('timeupdate', () => {
+  // Only update slider if user isn't dragging
+  if (!progress.matches(':active')) {
+    progress.value = track.currentTime;
+  }
+});
